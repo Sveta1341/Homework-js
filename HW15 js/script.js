3785a78a06315a9832d9b66b3d60017bf25b22a2
Sveta1341 <bsvetik1341@gmail.com>
2021-09-24 16:28:23 +0300

    add HW15

---
+++ HW15 js/script.js
@@ -0,0 +1,167 @@
+let canvas = document.getElementById("Canvas");
+let ctx = canvas.getContext("2d");
+let ballRadius = 10;
+let x = canvas.width / 2;
+let y = canvas.height - 30;
+let dx = 2;
+let dy = -2;
+let paddleHeight = 10;
+let paddleWidth = 75;
+let paddleX = (canvas.width - paddleWidth) / 2;
+let rightPressed = false;
+let leftPressed = false;
+let brickRowCount = 5;
+let brickColumnCount = 3;
+let brickWidth = 80;
+let brickHeight = 20;
+let brickPadding = 10;
+let brickOffsetTop = 30;
+let brickOffsetLeft = 30;
+let score = 0;
+let lives = 5;
+
+let bricks = [];
+for (var c = 0; c < brickColumnCount; c++) {
+	bricks[c] = [];
+	for (var r = 0; r < brickRowCount; r++) {
+		bricks[c][r] = { x: 0, y: 0, status: 1 };
+	}
+}
+
+document.addEventListener("keydown", keyDown, false);
+document.addEventListener("keyup", keyUp, false);
+document.addEventListener("mousemove", mouseMove, false);
+
+function keyDown(e) {
+	if (e.key == "Right" || e.key == "ArrowRight") {
+		rightPressed = true;
+	}
+	else if (e.key == "Left" || e.key == "ArrowLeft") {
+		leftPressed = true;
+	}
+}
+
+function keyUp(e) {
+	if (e.key == "Right" || e.key == "ArrowRight") {
+		rightPressed = false;
+	}
+	else if (e.key == "Left" || e.key == "ArrowLeft") {
+		leftPressed = false;
+	}
+}
+
+function mouseMove(e) {
+	let relativeX = e.clientX - canvas.offsetLeft;
+	if (relativeX > 0 && relativeX < canvas.width) {
+		paddleX = relativeX - paddleWidth / 2;
+	}
+}
+function collisionDetection() {
+	for (let c = 0; c < brickColumnCount; c++) {
+		for (let r = 0; r < brickRowCount; r++) {
+			let b = bricks[c][r];
+			if (b.status == 1) {
+				if (x > b.x && x < b.x + brickWidth && y > b.y && y < b.y + brickHeight) {
+					dy = -dy;
+					b.status = 0;
+					score++;
+					if (score == brickRowCount * brickColumnCount) {
+						alert("YOU WIN, CONGRATS!");
+						document.location.reload();
+					}
+				}
+			}
+		}
+	}
+}
+
+function drawBall() {
+	ctx.beginPath();
+	ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
+	ctx.fillStyle = "#33FFE2";
+	ctx.fill();
+	ctx.closePath();
+}
+function drawPaddle() {
+	ctx.beginPath();
+	ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight);
+	ctx.fillStyle = "#008080";
+	ctx.fill();
+	ctx.closePath();
+}
+function drawBricks() {
+	for (let c = 0; c < brickColumnCount; c++) {
+		for (let r = 0; r < brickRowCount; r++) {
+			if (bricks[c][r].status == 1) {
+				let brickX = (r * (brickWidth + brickPadding)) + brickOffsetLeft;
+				let brickY = (c * (brickHeight + brickPadding)) + brickOffsetTop;
+				bricks[c][r].x = brickX;
+				bricks[c][r].y = brickY;
+				ctx.beginPath();
+				ctx.rect(brickX, brickY, brickWidth, brickHeight);
+				ctx.fillStyle = "#008080";
+				ctx.fill();
+				ctx.closePath();
+			}
+		}
+	}
+}
+function drawScore() {
+	ctx.font = "16px Arial";
+	ctx.fillStyle = "#008080";
+	ctx.fillText("Score: " + score, 8, 20);
+}
+function drawLives() {
+	ctx.font = "16px Arial";
+	ctx.fillStyle = "#008080";
+	ctx.fillText("Lives: " + lives, canvas.width - 65, 20);
+}
+
+function drawGame() {
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	drawBricks();
+	drawBall();
+	drawPaddle();
+	drawScore();
+	drawLives();
+	collisionDetection();
+
+	if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
+		dx = -dx;
+	}
+	if (y + dy < ballRadius) {
+		dy = -dy;
+	}
+	else if (y + dy > canvas.height - ballRadius) {
+		if (x > paddleX && x < paddleX + paddleWidth) {
+			dy = -dy;
+		}
+		else {
+			lives--;
+			if (!lives) {
+				alert("GAME OVER");
+				document.location.reload();
+			}
+			else {
+				x = canvas.width / 2;
+				y = canvas.height - 30;
+				dx = 3;
+				dy = -3;
+				paddleX = (canvas.width - paddleWidth) / 2;
+			}
+		}
+	}
+
+	if (rightPressed && paddleX < canvas.width - paddleWidth) {
+		paddleX += 7;
+	}
+	else if (leftPressed && paddleX > 0) {
+		paddleX -= 7;
+	}
+
+	x += dx;
+	y += dy;
+	requestAnimationFrame(drawGame);
+}
+
+drawGame();
